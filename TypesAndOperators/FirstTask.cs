3c23968619918b1071af9cdf95ffda7709bf0632
Vitaliy Zhdanov <vitas579@mail.ru>
2023-11-24 13:10:31 +0500

    FirstTask finish commit

---
+++ TypesAndOperators/FirstTask.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+public class FirstTask
+{
+	public static void Main()
+    {
+        /*
+        UseCase:
+        1. 	Пользователь запускает программу
+        2. 	В терминале предлагается ввести число номер 1 – пользователь вводит число номер 1
+        3. 	В терминале предлагается ввести допустимую операцию(+, -, *, /) – пользователь вводит операцию
+        4. 	В терминале предлагается ввести число номер 2 – пользователь вводит число номер 2
+        5. 	В терминал выводится результат выбранной операции
+        */
+        float firstNumber;
+        float secondNumber;
+        string operetion;
+
+        Console.WriteLine("Введите первое число: ");
+        string? firstNumberString = Console.ReadLine();
+
+        Console.WriteLine("Введите операцию (+, -, /, *): ");
+        string? operation = Console.ReadLine();
+
+        Console.WriteLine("Введите второе число: ");
+        string? secondNumberString = Console.ReadLine();
+
+        firstNumber = float.Parse(firstNumberString, CultureInfo.InvariantCulture);
+        secondNumber = float.Parse(secondNumberString, CultureInfo.InvariantCulture);
+
+        switch (operation)
+        {
+            case "+":
+                Console.WriteLine($"Результат операции: {firstNumber + secondNumber}");
+                break;
+            case "-":
+                Console.WriteLine($"Результат операции: {firstNumber - secondNumber}");
+                break;
+            case "*":
+                Console.WriteLine($"Результат операции: {firstNumber * secondNumber}");
+                break;
+            case "/":
+                if (secondNumber == 0)
+                {
+                    Console.WriteLine("Нельзя делить на 0, иди в школу!");
+                }
+                else
+                {
+                    Console.WriteLine($"Результат операции: {firstNumber / secondNumber}");
+                }
+                break;
+            default:
+                Console.WriteLine("Введены недопустимые значения");
+                break;
+
+        }
+    }
+}
--- TypesAndOperators/Program.cs
+++ TypesAndOperators/Program.cs
@@ -1,142 +1,146 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Numerics;
+public class Program
 {
-    //Это сообщение будет выведено на экран
-    /*
-    Console.WriteLine("Hello, World!");
+    public static void Main1()
     {
-        Console.WriteLine("It's a nice day!");
-    }
-    */
-    /*
-    const string COMPANYNAME = "MTS";
-    string name;
-    name = "Zhdak";
-
-    Console.WriteLine("It's a nice day");
-    float d1 = 0.243f;
-    */
-
-
-    /*
-    //Console.WriteLine("Введите ваше имя:");
-    //string name = Console.ReadLine();
-    //Console.WriteLine($"Ваше имя: {name}");
-    //Console.WriteLine("Введите ваш возраст:");
-    //int age = Convert.ToInt32(Console.ReadLine());
-    //Console.WriteLine($"Ваш возраст: {age}");
-    */
-    /*
-        // Решение нашей задачи
-        // Подготовка данных
-        bool isWhiteBreadFresh = true;
-        ushort whiteBreadPrice = 150;
-        ushort butterPrice = 230;
-        ushort milkPrice = 170;
-        float milFatPercentage = 1.2f;
-        ushort icecreamPrice = 350;
-        Console.Write("Введите начальный баланс: ");
-        string? sumString = Console.ReadLine();
-        short sum = Convert.ToInt16(sumString);
-
-        if (sum > 0)
+        //Это сообщение будет выведено на экран
+        /*
+        Console.WriteLine("Hello, World!");
         {
-            if (isWhiteBreadFresh)
+            Console.WriteLine("It's a nice day!");
+        }
+        */
+        /*
+        const string COMPANYNAME = "MTS";
+        string name;
+        name = "Zhdak";
+
+        Console.WriteLine("It's a nice day");
+        float d1 = 0.243f;
+        */
+
+
+        /*
+        //Console.WriteLine("Введите ваше имя:");
+        //string name = Console.ReadLine();
+        //Console.WriteLine($"Ваше имя: {name}");
+        //Console.WriteLine("Введите ваш возраст:");
+        //int age = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine($"Ваш возраст: {age}");
+        */
+        /*
+            // Решение нашей задачи
+            // Подготовка данных
+            bool isWhiteBreadFresh = true;
+            ushort whiteBreadPrice = 150;
+            ushort butterPrice = 230;
+            ushort milkPrice = 170;
+            float milFatPercentage = 1.2f;
+            ushort icecreamPrice = 350;
+            Console.Write("Введите начальный баланс: ");
+            string? sumString = Console.ReadLine();
+            short sum = Convert.ToInt16(sumString);
+
+            if (sum > 0)
             {
-                if (sum >= whiteBreadPrice)
+                if (isWhiteBreadFresh)
                 {
-                    sum -= (short)whiteBreadPrice;
-                    Console.WriteLine($"Куплен свежий батон за {whiteBreadPrice} рублей");
+                    if (sum >= whiteBreadPrice)
+                    {
+                        sum -= (short)whiteBreadPrice;
+                        Console.WriteLine($"Куплен свежий батон за {whiteBreadPrice} рублей");
+                    }
+                    else
+                    {
+                        Console.WriteLine("На батон денег нет :(");
+                    }
+
                 }
                 else
                 {
-                    Console.WriteLine("На батон денег нет :(");
+                    Console.WriteLine("Батон не свежий");
                 }
 
+                sum += (short)butterPrice;
+
+                if (milFatPercentage == 1.2f)
+                {
+                    sum -= (short)milkPrice;
+                }
+
+                if (sum >= icecreamPrice)
+                {
+                    sum -= (short)icecreamPrice;
+                }
+
+                Console.WriteLine($"Остаток суммы: {sum}");
             }
             else
             {
-                Console.WriteLine("Батон не свежий");
+                Console.WriteLine("Деняк не дали :(");
             }
-
-            sum += (short)butterPrice;
-
-            if (milFatPercentage == 1.2f)
-            {
-                sum -= (short)milkPrice;
-            }
-
-            if (sum >= icecreamPrice)
-            {
-                sum -= (short)icecreamPrice;
-            }
-
-            Console.WriteLine($"Остаток суммы: {sum}");
+        */
+        /*
+        int x = 10;
+        int y = 20;
+        int z;
+        if (x < y)
+        {
+            z = y + x;
         }
         else
         {
-            Console.WriteLine("Деняк не дали :(");
+            z = x - y;
         }
-    */
-    /*
-    int x = 10;
-    int y = 20;
-    int z;
-    if (x < y)
-    {
-        z = y + x;
-    }
-    else
-    {
-        z = x - y;
-    }
 
-    var z1 = x < y ? (x + y) : (x - y);
-    Console.WriteLine(z);
-    Console.WriteLine(z1);
-    */
-    /*
-    int x = 1;
-    switch (x)
-    {
-        case 1:
-            Console.WriteLine("1");
-            break;
-        case 2:
-            Console.WriteLine("2");
-            break;
-       default:
-            Console.WriteLine("Отрицательно");
-            break;
-    }
-    */
-    string name = "Tom";
-    switch (name)
-    {
-        case "Alex":
-            Console.WriteLine("Alex");
-            break;
-        case "Tom":
-            Console.WriteLine("Tom");
-            break;
-    }
-
-    int DoOperation(int x)
-
-    {
+        var z1 = x < y ? (x + y) : (x - y);
+        Console.WriteLine(z);
+        Console.WriteLine(z1);
+        */
+        /*
+        int x = 1;
         switch (x)
         {
             case 1:
-                return 1;
+                Console.WriteLine("1");
+                break;
             case 2:
-                return 2;
-            default: 
-                return 0;
-
+                Console.WriteLine("2");
+                break;
+           default:
+                Console.WriteLine("Отрицательно");
+                break;
         }
+        */
+        string name = "Tom";
+        switch (name)
+        {
+            case "Alex":
+                Console.WriteLine("Alex");
+                break;
+            case "Tom":
+                Console.WriteLine("Tom");
+                break;
+        }
+
+        int DoOperation(int x)
+
+        {
+            switch (x)
+            {
+                case 1:
+                    return 1;
+                case 2:
+                    return 2;
+                default:
+                    return 0;
+
+            }
+        }
+        Console.WriteLine(DoOperation(2));
     }
-    Console.WriteLine(DoOperation(2));
 
 }
 

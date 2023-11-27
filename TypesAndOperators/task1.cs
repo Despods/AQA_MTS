df2df890aaf2ea2615dc38ec749b438c5fd12a41
Vitaliy Zhdanov <vitas579@mail.ru>
2023-11-27 22:15:46 +0500

    Task1 Finish

--- TypesAndOperators/task1.cs
+++ TypesAndOperators/task1.cs
@@ -1,8 +1,26 @@
 ﻿using System;
 
-public class Class1
+public class Task1
 {
-	public Class1()
+	static void Main()
 	{
+		/*
+		Создайте массив целых чисел. 
+		Напишете программу, которая выводит сообщение о том, входит ли заданное число в массив или нет. 
+		Пусть число для поиска задается с консоли.
+		*/
+		int[] array1 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }; //Создаем массив из 8 чисел
+		Console.WriteLine("Введите число для поиска его в массиве: ");
+		int numberFind = Convert.ToInt32(Console.ReadLine()); //считаваем строку и преобразовываем в инт
+
+		if (array1.Contains(numberFind)) //Проверяем входит ли число в массив встроенным методом массива
+		{
+            Console.WriteLine($"Число '{numberFind}' входит в массив!");
+        }else
+		{
+            Console.WriteLine($"Число '{numberFind}' не входит в массив!");
+        }
+		
+
 	}
 }

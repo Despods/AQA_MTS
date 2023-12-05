245331cab558d36001ec8ba031250dabc7899062
Vitaliy Zhdanov <vitas579@mail.ru>
2023-12-05 23:28:19 +0500

    task1 finish

---
+++ TypesAndOperators/task1.cs
@@ -0,0 +1,29 @@
+﻿public class Task1
+{
+    static void Main1()
+    {
+        /*
+        Заменить в строке все вхождения 'test' на 'testing'. 
+        Удалить из текста все символы, являющиеся цифрами.
+        */
+
+        string text = "test TEST TeSt tested test1234 dotest";//инициализируем переменную
+
+        Console.WriteLine(text);//выведем исходную переменную
+
+        string replacedText = text.Replace("test", "testing");//меняем все test на testing
+
+        Console.WriteLine(replacedText);
+
+        string removedNumber = "";//инициализируем переменную, в которую положим исходный текст без цифр
+        foreach (char d in replacedText)//бежим по массиву, если символ равен числу - меняем его на пустое символ. используя метод символа IsDigit
+        {
+            if (Char.IsDigit(d))
+                removedNumber += "";
+            else
+                removedNumber += d;
+        }
+
+        Console.WriteLine(removedNumber);
+    }
+}

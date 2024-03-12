f703132cda00be254f90f27f506f214a4b212529
Vitaliy Zhdanov <vitas579@mail.ru>
2024-03-12 21:53:40 +0500

    task1 done

---
+++ Task1/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Book
+    {
+        public string name;
+        public string author;
+        public int yearPublication;
+
+        public Book(string name, string author, int yearPublication)
+        {
+            this.name = name;
+            this.author = author;
+            this.yearPublication = yearPublication;
+        }
+
+        public void ShowInformation()
+        {
+            Console.WriteLine("Название книги: {0}", name);
+            Console.WriteLine("Автор: {0}", author);
+            Console.WriteLine("Год публикации: {0}", yearPublication);
+        }
+
+    }
+}

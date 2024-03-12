80d27335e9a821c53b10a8eba9af6ba9669fb720
Vitaliy Zhdanov <vitas579@mail.ru>
2024-03-12 22:45:01 +0500

    task2 done

--- Task1/Book.cs
+++ Task1/Book.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Task1
+﻿namespace Task1
 {
     public class Book
     {
@@ -18,13 +12,11 @@ namespace Task1
             this.author = author;
             this.yearPublication = yearPublication;
         }
-
         public void ShowInformation()
         {
             Console.WriteLine("Название книги: {0}", name);
             Console.WriteLine("Автор: {0}", author);
             Console.WriteLine("Год публикации: {0}", yearPublication);
         }
-
     }
 }
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -9,94 +9,96 @@
 */
 using System.Collections;
 using Task1;
-using static System.Reflection.Metadata.BlobBuilder;
-
-ArrayList Book = new ArrayList();
-int command;
-while (true)
+internal class Program
 {
-    Console.WriteLine(@"Библиотека
+    private static void Main()
+    {
+        ArrayList Book = new ArrayList();
+        int command;
+        while (true)
+        {
+            Console.WriteLine(@"Библиотека
 1. Добавление книги
 2. Отображение списка книг
 3. Поиск книги по автору
 4. Удаление книги
 5. Выход из программы");
-    Console.WriteLine();
-    Console.WriteLine("Выберите необходимый пункт меню:");
-    command = Convert.ToInt32(Console.ReadLine());
-    switch (command)
-    {
-        case 1:
-            Console.Clear();
-            Console.WriteLine("Добавление книги");
-            Console.WriteLine("Введите название книги");
-            string name = Console.ReadLine();
-            Console.WriteLine("Введите автора книги");
-            string author = Console.ReadLine();
-            Console.WriteLine("Введите год издания книги");
-            int year = Convert.ToInt32(Console.ReadLine());
-
-            Book book = new Book(name, author, year);
-            Book.Add(book);
-            Console.WriteLine("Успешно добавлена книга");
-            break;
-        case 2:
-            Console.Clear();
-            Console.WriteLine("Отображение списка книг");
-            if (Book.Count == 0)
-            {
-                Console.WriteLine("Книг нет");
-            }
-            else
-            {
-                Console.WriteLine("Список книг");
-                foreach (Book i in Book)
-                {
-                    i.ShowInformation();
-                }
-            }
-            break;
-        case 3:
-            Console.Clear();
-            Console.WriteLine("Поиск книги по автору");
             Console.WriteLine();
-            Console.WriteLine("Введите автора");
-            string searchAythor= Console.ReadLine();
-            bool flag = false;
-
-            foreach (Book i in Book)
+            Console.WriteLine("Выберите необходимый пункт меню:");
+            command = Convert.ToInt32(Console.ReadLine());
+            switch (command)
             {
-                if (i.author == searchAythor)
-                {
-                    i.ShowInformation();
-                    flag = true;
-                }
-                else Console.WriteLine("По данному автору книга не найдена");
-            }
-            break;
-        case 4:
-            Console.Clear();
-            Console.WriteLine("Удаление книги");
-            Console.WriteLine("Введите название книги, которую необходимо удалить");
-            string nameBookDelete = Console.ReadLine();
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("Добавление книги");
+                    Console.WriteLine("Введите название книги");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Введите автора книги");
+                    string author = Console.ReadLine();
+                    Console.WriteLine("Введите год издания книги");
+                    int year = Convert.ToInt32(Console.ReadLine());
 
-            foreach (Book i in Book)
-            {
-                if (i.name == nameBookDelete)
-                {
-                    Book.Remove(i);
-                    Console.WriteLine("Книга была удалена");
+                    Book book = new Book(name, author, year);
+                    Book.Add(book);
+                    Console.WriteLine("Успешно добавлена книга");
                     break;
-                }
-                else Console.WriteLine("По данному названию книга не найдена"); ;
+                case 2:
+                    Console.Clear();
+                    Console.WriteLine("Отображение списка книг");
+                    if (Book.Count == 0)
+                    {
+                        Console.WriteLine("Книг нет");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Список книг");
+                        foreach (Book i in Book)
+                        {
+                            i.ShowInformation();
+                        }
+                    }
+                    break;
+                case 3:
+                    Console.Clear();
+                    Console.WriteLine("Поиск книги по автору");
+                    Console.WriteLine();
+                    Console.WriteLine("Введите автора");
+                    string searchAythor = Console.ReadLine();
+
+                    foreach (Book i in Book)
+                    {
+                        if (i.author == searchAythor)
+                        {
+                            i.ShowInformation();
+                        }
+                        else Console.WriteLine("По данному автору книга не найдена");
+                    }
+                    break;
+                case 4:
+                    Console.Clear();
+                    Console.WriteLine("Удаление книги");
+                    Console.WriteLine("Введите название книги, которую необходимо удалить");
+                    string nameBookDelete = Console.ReadLine();
+
+                    foreach (Book i in Book)
+                    {
+                        if (i.name == nameBookDelete)
+                        {
+                            Book.Remove(i);
+                            Console.WriteLine("Книга была удалена");
+                            break;
+                        }
+                        else Console.WriteLine("По данному названию книга не найдена"); ;
+                    }
+                    break;
+                case 5:
+                    Console.Clear();
+                    Console.WriteLine("Выход из программы");
+                    return;
+                default:
+                    Console.WriteLine("Неверная команда");
+                    break;
             }
-            break;
-        case 5:
-            Console.Clear();
-            Console.WriteLine("Выход из программы");
-            return;
-        default:
-            Console.WriteLine("Неверная команда");
-            break;
+        }
     }
 }

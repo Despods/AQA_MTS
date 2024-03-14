a8c452b52bca46b3eee249f9dab260c93668ace5
Vitaliy Zhdanov <vitas579@mail.ru>
2024-03-14 20:57:07 +0500

    generic homework done

---
+++ Task1/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Point<T>
+    {
+        private T x;
+        private T y;
+
+        public T X { get { return this.x; } }
+        public T Y { get { return this.y; } }
+
+        public Point(T x, T y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Координаты: X = {x}, Y = {y}.");
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Реализовать класс Point, который определяет точку на координатной плоскости. В классе реализовать:
+внутренние поля x, y;
+конструктор с 2 параметрами;
+свойства доступа к внутренним полям класса;
+метод, выводящий значения внутренних полей класса.
+*/
+using System.Drawing;
+using Task1;
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Point<int> pointInt = new Point<int>(10, 15);
+        Point<double> pointDouble = new Point<double>(5.2, 10.5);
+
+        pointInt.PrintInfo();
+        pointDouble.PrintInfo();
+    }
+}

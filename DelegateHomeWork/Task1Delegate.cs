dc99f0dce247c36efb4c2d73efa892015f9ba307
Vitaliy Zhdanov <vitas579@mail.ru>
2024-03-16 18:33:18 +0500

    delegateGomeWork DOne

---
+++ DelegateHomeWork/Task1Delegate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateHomeWork
+{
+    internal class Task1Delegate
+    {
+        public delegate int TaskOneDelegate();
+
+        public void Run()
+        {
+            TaskOneDelegate taskOneDelegate = new(RandomNumber);
+            Console.WriteLine($"Число рандом от 0 до 5 - {taskOneDelegate()}");
+        }
+
+        private int RandomNumber()
+        {
+            Random random = new Random();
+            return random.Next(0,11);
+        }
+    }
+}

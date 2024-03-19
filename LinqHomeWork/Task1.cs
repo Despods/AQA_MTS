85dfb8ef921b8ae37e0f31692bfe03aefe836158
Vitaliy Zhdanov <vitas579@mail.ru>
2024-03-19 23:49:16 +0500

    linq done homework

---
+++ LinqHomeWork/Task1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqHomeWork
+{
+    internal class Task1
+    {
+        public void Run()
+        {
+            Console.WriteLine("Изначальная коллекция");
+            var listNumbers = new List<int>() { -4, -4, -3, -2, -2, -1, 0, 1, 2, 1, 2, 3, 4, 5, 4, 5 };
+            var resultNumbers = listNumbers.Where(number => Math.Abs(number) % 2 == 1).Distinct();
+            PrintHelper.Print(resultNumbers);
+        }
+    }
+}
---
+++ LinqHomeWork/Task3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqHomeWork
+{
+    internal class Task3
+    {
+        public void Run() 
+        {
+            List<string> strings = new List<string>() 
+            { "BOB", "MARLEY", "SAM", "KATE", "YURKA", "MAX", "SAME", "ALEX" };
+
+            Console.WriteLine("Изначальная коллекция");
+            PrintHelper.Print(strings);
+
+            var sortStrings =
+                (from i in strings
+                 orderby i descending
+                 select i).OrderBy(i => i.Length);
+
+            Console.WriteLine("Отсортированная коллекция");
+            PrintHelper.Print(sortStrings);
+        }
+    }
+}
